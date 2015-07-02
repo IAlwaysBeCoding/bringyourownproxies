550330869a7371b249b3c197c6809e7b7afb96be
IAlwaysBeCoding <tedwantsmore@gmx.com>
2015-07-02 17:13:57 -0400

    fixed some porn issues

--- bringyourownproxies/test_injectorhttpconnection.py
+++
@@ -1,6 +0,0 @@
-import InjectorHttpConnection
-
-import requests
-print requests.get('http://www.google.com')
-r = requests.post('https://www.google.com',data={'lol':'dfd','dfd':'dd'})
-print r.request.headers
