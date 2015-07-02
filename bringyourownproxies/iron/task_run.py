1be30f27d532c0aa8f3ea91b735f6c0cf055add6
IAlwaysBeCoding <tedwantsmore@gmx.com>
2015-07-02 17:20:13 -0400

    removed unnecessary folders

--- bringyourownproxies/iron/task_run.py
+++
@@ -1,4 +0,0 @@
-from tasks import display_msg
-
-while True:
-    print display_msg.delay(4)
--- bringyourownproxies/uploader/errors.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/python
-
-class UnknownUploadEvent(Exception):
-    pass
-
